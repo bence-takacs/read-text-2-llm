99021add4ee782ac62b0bb130e7c290460184025
Bence Takács <takacs.bence@gmail.com>
2023-06-13 21:57:00 +0200

    fix logging + upload location

--- embedding.py
+++ embedding.py
@@ -6,19 +6,20 @@ from langchain.chains import RetrievalQA
 from langchain.document_loaders import TextLoader
 from langchain.evaluation.qa import QAEvalChain
 import json
+import logging
 
 # See: https://github.com/hwchase17/langchain/blob/b95002289409077965d99636b15a45300d9c0b9d/docs/use_cases/evaluation/data_augmented_question_answering.ipynb
 
 class Embedding:
   def __init__(self, text_file):
-    print('--- Loading text ---')
+    logging.info('--- Loading text ---')
     loader = TextLoader(text_file)
     documents = loader.load()
-    print('--- Splitting text into chunks ---')
+    logging.info('--- Splitting text into chunks ---')
     text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0) # TokenTextSplitter(chunk_size=1000, chunk_overlap=0) #
     texts = text_splitter.split_documents(documents)
 
-    print('--- Init OpenAI embedding API ---')
+    logging.info('--- Init OpenAI embedding API ---')
     embeddings = OpenAIEmbeddings()
     docsearch = Chroma.from_documents(texts, embeddings)
     self.qa = RetrievalQA.from_llm(llm=OpenAI(), retriever=docsearch.as_retriever()) #model='text-davinci-003'
@@ -35,9 +36,8 @@ class Embedding:
 # eval_chain = QAEvalChain.from_llm(llm)
 # graded_outputs = eval_chain.evaluate(examples, predictions)
 # for i, eg in enumerate(examples):
-#     print(f"Example {i}:")
-#     print("Question: " + predictions[i]['query'])
-#     print("Real Answer: " + predictions[i]['answer'])
-#     print("Predicted Answer: " + predictions[i]['result'])
-#     print("Predicted Grade: " + graded_outputs[i]['text'])
-#     print()
+#     logging.info(f"Example {i}:")
+#     logging.info("Question: " + predictions[i]['query'])
+#     logging.info("Real Answer: " + predictions[i]['answer'])
+#     logging.info("Predicted Answer: " + predictions[i]['result'])
+#     logging.info("Predicted Grade: " + graded_outputs[i]['text'])
--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from flask import Flask, render_template, request, redirect, url_for
 from werkzeug.utils import secure_filename
 from embedding import Embedding
 import os
+import logging
 
 
 class MyApplication:
     def __init__(self):
+        logging.basicConfig(level=logging.INFO)
         self.app = Flask(__name__)
-        self.app.config['UPLOAD_FOLDER'] = './uploads'
+        self.app.config['UPLOAD_FOLDER'] = './.uploads'
         self.app.config['SECRET_KEY'] = 'your-secret-key'  # replace with a real secret key
 
         if not os.path.exists(self.app.config['UPLOAD_FOLDER']):
@@ -25,9 +27,12 @@ class MyApplication:
         @self.app.route('/', methods=['POST'])
         def save_file():
             file = request.files['file']
+            logging.info(f"Uploading file [ {file.filename}  | { file.content_type } | {file.content_length} ]")
             filename = secure_filename(file.filename)
             filepath=os.path.join(self.app.config['UPLOAD_FOLDER'], filename)
             file.save(filepath)
+            filesize=os.stat(filepath).st_size
+            logging.info(f"Size: {filesize} bytes")
             self.embedding = Embedding(filepath)
             return redirect(url_for('chat'))
 
@@ -38,8 +43,9 @@ class MyApplication:
         @self.app.route('/chat', methods=['POST'])
         def respond_to_chat():
             user_message = request.form['message']
-            # TODO: process the user's message and generate a response
+            logging.info(f"Question: {user_message}")
             response = self.embedding.query(user_message) if self.embedding else "You should initialize knowledge base first"
+            logging.info(f"Answer: {response}")
             return render_template('chat.html', response=response)
         
     def run(self):
