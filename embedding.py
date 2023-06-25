8ffbf3273068a90b9dcbdcc93fa2879ebed7765e
Bence Takács <takacs.bence@gmail.com>
2023-06-25 15:23:56 +0200

    fix db

--- embedding.py
+++ embedding.py
@@ -4,12 +4,13 @@ from langchain.text_splitter import  CharacterTextSplitter # TokenTextSplitter #
 from langchain.llms import OpenAI
 from langchain.chains import RetrievalQA
 from langchain.document_loaders import TextLoader
-from langchain.evaluation.qa import QAEvalChain
+from langchain.chat_models import ChatOpenAI
 import json
 import logging
 
 # See: https://github.com/hwchase17/langchain/blob/b95002289409077965d99636b15a45300d9c0b9d/docs/use_cases/evaluation/data_augmented_question_answering.ipynb
 
+CHROMA_DB_DIRECTORY = './.chroma'
 class Embedding:
   def __init__(self, text_file):
     logging.info('--- Loading text ---')
@@ -21,8 +22,21 @@ class Embedding:
 
     logging.info('--- Init OpenAI embedding API ---')
     embeddings = OpenAIEmbeddings()
-    docsearch = Chroma.from_documents(texts, embeddings)
-    self.qa = RetrievalQA.from_llm(llm=OpenAI(), retriever=docsearch.as_retriever()) #model='text-davinci-003'
+    docsearch = Chroma.from_documents(
+      texts, 
+      embeddings, 
+      persist_directory=CHROMA_DB_DIRECTORY
+      #     collection_name="ask_django_docs",
+    )
+
+    docsearch.persist()
+    self.qa = RetrievalQA.from_chain_type(
+      llm=ChatOpenAI(temperature=0), 
+      chain_type="stuff",
+      retriever=docsearch.as_retriever(),
+      chain_type_kwargs={"verbose": True}
+      #model='text-davinci-003'
+    ) 
 
   def query(self, question):
     return self.qa.run(question)
--- set_me_up.sh
+++ set_me_up.sh
@@ -6,3 +6,5 @@ source .local_settings
 
 PYTHONPATH=src pytest
 
+echo "In order to set up the app, you should run:"
+echo "export OPENAI_API_KEY=$OPENAI_API_KEY"
